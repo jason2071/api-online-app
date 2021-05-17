80608688248bcf5cbf4b69e33eb21ecd2b335e8b
Phumai <mcc3939@gmail.com>
2021-05-17 16:26:06 +0700

    add react-app

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   ENV: process.env.NODE_ENV || "development",
-  PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT || 5000,
   URL: process.env.BASE_URL || "http://localhost:3000",
   MONGODB_URL:
     process.env.MONGODB_URL ||
